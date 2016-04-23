4eec93b4fe36812d902be78843a9a2888e79b963
jimczj <jimczj@gmail.com>
2016-04-24 00:27:25 +0800

    增加migrations

--- stucampus/articles/views.py
+++ stucampus/articles/views.py
@@ -185,9 +185,7 @@ def article_display(request, id=None):
     article.click_count += 1
     article.save()
     comments=DuoShuo.getListPosts(article.id)
-    commentAndLikeNum = DuoShuo.getCommentsAndLikesNum(article.id)
-    article.comments = commentAndLikeNum['comments']
-    article.likes = commentAndLikeNum['likes']
+    article = DuoShuo.appendNumToArticle(article)
     return render(request, 'articles/article-display.html',
             {'article': article,'comments':comments})
 
